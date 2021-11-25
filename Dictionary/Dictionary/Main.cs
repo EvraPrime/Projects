9de387c3c9ac6b8b00c649401663709c793facb0
EvraPrime <62198187+EvraPrime@users.noreply.github.com>
2021-11-26 01:19:29 +0700

    new project
    Dictionary with minigame

---
+++ Dictionary/Dictionary/Main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dictionary.Helper;
+
+namespace Dictionary
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (panel1.Controls.Count == 0)
+                Animation.slideToDestination(this, panel1, sender as Control, Animation.Position.Center, 1, () => AddToPanel(sender));
+            else
+                Animation.slideToDestination(this, panel1, sender as Control, Animation.Position.Bottom, 1, () => AddToPanel(sender));
+        }
+
+        private void AddToPanel(object sender)
+        {
+            if (panel1.InvokeRequired)
+            {
+                Action safeWrite = delegate { AddToPanel(sender); };
+                panel1.Invoke(safeWrite);
+            }
+            else
+            {
+                panel1.Controls.Add(sender as Control);
+                
+            }
+        }
+    }
+}
